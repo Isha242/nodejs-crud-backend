7ad04300b0e9206f1724f9a285efde6a4771e00d
Isha Dash <71626410+Isha242@users.noreply.github.com>
2024-10-04 23:51:09 +0530

    changes

---
+++ routers/userRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  checkInput,
+  searchUserByParams,
+  forgetPassword,
+  resetPassword,
+} = require("../controllers/userController");
+
+userRouter.get("/", searchUserByParams);
+userRouter.get("/", getUsers);
+userRouter.post("/", createUser);
+userRouter.get("/:id", getUserById);
+userRouter.patch("/:id", updateUserById);
+userRouter.delete("/:id", deleteUserById);
+userRouter.post("/forgetPassword", forgetPassword);
+userRouter.patch("/resetPassword/:userId", resetPassword);
+
+module.exports = userRouter;
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRouter = require("./routers/userRouter");
+// const productRouter = require("./routers/productRouter");
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3300;
+const DB_URL = process.env.DB_URL;
+
+/**Database connection starts */
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Connection to MongoDB is established!");
+  })
+  .catch((err) => {
+    console.log("something went wrong with DB connection", err);
+  });
+/**Database connection ends */
+
+const app = express();
+app.use(express.json());
+
+/**routers*/
+app.use("/api/user", userRouter);
+// app.use("/api/product", productRouter);
+
+//fallback middleware, i.e if no middleware works then the default middleware works
+app.use(function (req, res) {
+  res.status(404).send("404 Not Found");
+});
+
+//start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost/${PORT}`);
+});
